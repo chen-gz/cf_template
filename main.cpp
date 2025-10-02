89bccc57f5f616452f8bd477c7b6cb8a718fccc8
Guangzong Chen <chen@ggeta.com>
2025-10-01 21:34:15 -0700

    update main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
-#include <tuple>
-#include <vector>
-#include <iostream>
+#include <algorithm>
+#include <cmath>
 #include <cstdint>
+#include <iostream>
+#include <numeric>
 #include <set>
+#include <tuple>
+#include <map>
+#include <vector>
 using namespace std;
 #define int long long
+// #define mod 1000000007
 
 #define N 500005
 
 void solve() {
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;  
     vector<int> a(n);
+    map<int, int> cnt;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
+        cnt[a[i]]++;
     }
-    cout << min(a[0] + a[0], a[0] + a[1]) << endl;
+    int ans = 0;
+    for(int i=0;i<k;i++){
+        if (cnt[i] == 0) {
+            ans++;
+        }
+    }
+    if (ans < cnt[k]) {
+        ans += cnt[k] - ans;
+    }
+    cout << ans << endl;
 }
 
 int32_t main() {
