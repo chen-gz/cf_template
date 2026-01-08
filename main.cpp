de34e89f4ef78771dc6fbac782eab4ca17a5d902
Guangzong Chen <chen@ggeta.com>
2026-01-07 19:16:11 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,53 +1,42 @@
 #include <algorithm>
+#include <bit>
+#include <bitset>
 #include <cmath>
 #include <cstdint>
+#include <cstdio>
 #include <iostream>
-#include <numeric>
-#include <set>
-#include <tuple>
 #include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
 #include <vector>
-using namespace std;
-#define int long long
 // #define mod 1000000007
 
-#define N 500005
+#define N 100006
+#define mod 998244353
+using namespace std;
 
 void solve() {
-    int n, k;
-    cin >> n >> k;  
-    vector<int> a(n);
-    map<int, int> cnt;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        cnt[a[i]]++;
-    }
-    int ans = 0;
-    for(int i=0;i<k;i++){
-        if (cnt[i] == 0) {
-            ans++;
-        }
-    }
-    if (ans < cnt[k]) {
-        ans += cnt[k] - ans;
-    }
-    cout << ans << endl;
+
 }
 
 int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-#ifdef DEBUG
-    freopen("./input.txt", "r", stdin);
-#endif
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    #ifdef DEBUG
+        freopen("./input.txt", "r", stdin);
+    #endif
     int t;
-    cin >> t;
+    std::cin >> t;
     while (t--) {
 #ifdef DEBUG
         static int test_num = 1;
-        cout << "test case: " << test_num++ << endl;
+        std::cout << "test case: " << test_num++ << std::endl;
 #endif
         solve();
     }
+    return 0;
 }
